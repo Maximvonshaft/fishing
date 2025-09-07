ec9e3ab41c8f5a5b4e5199a9465375db5d23cce8
Maximvonshaft <bojackhorseman@88.com>
2025-09-08 00:04:40 +0800

    Split CSS and JS into external files

---
+++ tailwind-config.js
@@ -0,0 +1,1 @@
+tailwind.config={darkMode:'class',theme:{container:{center:true,padding:'1rem'},extend:{fontFamily:{display:['Inter','ui-sans-serif','system-ui'],sans:['Inter','ui-sans-serif','system-ui']}}}};
